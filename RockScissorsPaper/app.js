7ad7a51360a1de0fba457ff68a2c485f5761ceac
Abraham Gutiérrez Chávez <abguch22@gmail.com>
2023-01-10 15:59:51 -0600

    finish first game

--- RockScissorsPaper/app.js
+++ RockScissorsPaper/app.js
@@ -4,12 +4,15 @@ const resultDisplay =  document.getElementById('result');
 
 const possibleChoices = document.querySelectorAll('button');
 let userChoice; 
+let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>  {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
 
-    generateComputerChoice()
+    generateComputerChoice();
+    gerResult();
 }));
 
 function generateComputerChoice() {
@@ -27,4 +30,30 @@ function generateComputerChoice() {
         computerChoice = 'paper'
     }
     computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+
+function gerResult(){
+    if(computerChoice === userChoice){
+        result = 'its a draw';
+    }
+    if(computerChoice === 'rock' && userChoice === 'paper'){
+        result = 'you win';
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors'){
+        result = 'you lose';
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors'){
+        result = 'you win';
+    }
+    if(computerChoice === 'paper' && userChoice === 'rock'){
+        result = 'you lose';
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock'){
+        result = 'you win';
+    }
+    if(computerChoice === 'scissors' && userChoice === 'paper'){
+        result = 'you lose';
+    }
+    resultDisplay.innerHTML = result;
 }
