c739c8d6d23294ff7bd409d8ad0853d5173756c4
Abraham Gutiérrez Chávez <abguch22@gmail.com>
2023-01-26 11:39:12 -0600

    finish 3rd game whac-a-mole

--- Whac-a-mole/app.js
+++ Whac-a-mole/app.js
@@ -4,15 +4,50 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 5
 
 function randomSquare(){
     squares.forEach(square => {
         square.classList.remove('mole')
     })
-
     
-    let randomPosition = squares[Math.floor(Math.random() * 9)]
-    console.log(randomPosition);
+    let randomNumber = Math.floor(Math.random() * 9)
+    let randomSquare = squares[randomNumber]
+
+    randomSquare.classList.add('mole')
+    hitPosition = randomSquare.id
 }
 
-randomSquare()
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+// ! ↓ function working ↓
+        moveMole()
+// ! ↑ function working ↑
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert("GAME OVER! Your final score is " + result)
+    }
+
+}
+
+let countDownTimerId = setInterval(countDown,1000);
